cd5ff7adc37540003ea0e8af6cc837cceca7691c
Maxwell Sturdivant <109681165+msturdi@users.noreply.github.com>
2025-07-09 14:50:22 -0500

    Update stackbar.order.16S.R

--- 2023 Cotton-growing season (Stiles Farm only)/stackbar.order.16S.R
+++ 2023 Cotton-growing season (Stiles Farm only)/stackbar.order.16S.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(readxl)
 library(ggtext)
-library(RColorBrewer)
 library(MoMAColors)
 library(glue)
 library(dplyr)
@@ -47,7 +46,7 @@ order_rel_abund <- otu_rel_abund %>%
   mutate(taxon = str_replace(taxon,
                              "(.*)_unclassified", "Unclassified \\1"),
          taxon = str_replace_all(taxon, 
-                                 "_", " ")) #gets ride of _ in taxon names
+                                 "_", " "))
 
 order_pool <- order_rel_abund %>%
   group_by(taxon) %>%
@@ -63,7 +62,7 @@ inner_join(order_rel_abund, order_pool, by="taxon") %>%
             .groups="drop") %>%
   mutate(taxon = factor(taxon),
          taxon = fct_reorder(taxon, mean, .desc=TRUE),
-         taxon = fct_shift(taxon, n=1)) %>% #all of this mutate step is creating an "anchor" at the top and the bottom of the figure using the 2 most abundant phyla
+         taxon = fct_shift(taxon, n=1)) %>%
   ggplot(aes(x=sample, y=rel_abund, fill=taxon)) +
   geom_col() +
   scale_fill_manual(name="Orders",
@@ -75,8 +74,7 @@ inner_join(order_rel_abund, order_pool, by="taxon") %>%
                              "Acidobacteria Gp6 order incertae sedis", "Solirubrobacterales", "Rhodospirillales", "Acidimicrobiales",
                              "Alphaproteobacteria order incertae sedis", "Sphingomonadales", "**Burkholderiales***", "Sphingobacteriales", 
                              "Pseudomonadales", "Actinomycetales", "Other"),
-                    #values = c(brewer.pal(12, "Paired"), "navyblue", "dimgrey")) +
-                    values = c(moma.colors("Warhol", 15, direction=1, type="continuous"), "dimgrey")) + #type could be continuous
+                    values = c(moma.colors("Warhol", 15, direction=1, type="continuous"), "dimgrey")) +
   scale_x_discrete(limits=c("AA1B", "AB1B", "AA2B", "AB2B", "AA3B", "AB3B", "BA1B", "BB1B", "BA2B", "BB2B", "BA3B", "BB3B", 
                             "CA1B", "CB1B", "CA2B", "CB2B", "CA3B", "CB3B", 
                             "BAAB", "BABB", "BBAB", "BBBB"),
@@ -85,7 +83,7 @@ inner_join(order_rel_abund, order_pool, by="taxon") %>%
                             "Heat<br>Killed<br>1", "Heat<br>Killed<br>2", "Heat<br>Killed<br>3", "Heat<br>Killed<br>4", "Heat<br>Killed<br>5", 
                             "Heat<br>Killed<br>6",
                             "Bulk<br>Soil<br>1", "Bulk<br>Soil<br>2", "Bulk<br>Soil<br>3", "Bulk<br>Soil<br>4")) +
-  scale_y_continuous(expand=c(0,0)) + #this makes the bars touch the y axis
+  scale_y_continuous(expand=c(0,0)) +
   labs(x=NULL,
        y="Relative Abundance (%)",
        title="Order-level 16S rRNA Gene Alpha Diversity") +
@@ -99,33 +97,3 @@ inner_join(order_rel_abund, order_pool, by="taxon") %>%
         plot.title = element_text(hjust=0.5))
 
 ggsave("order.stackedbar.16S.png", width=12, height=5)
-
-#class-level
-class_rel_abund <- otu_rel_abund %>%
-  filter(level=="class") %>%
-  group_by(trt, sample, taxon) %>%
-  summarize(rel_abund = sum(rel_abund), 
-            .groups="drop") %>%
-  group_by(trt, taxon) %>%
-  mutate(taxon = str_replace(taxon,
-                             "(.*)_unclassified", "Unclassified \\1"),
-         taxon = str_replace_all(taxon, 
-                                 "_", " ")) #gets ride of _ in taxon names
-
-class_pool <- class_rel_abund %>%
-  group_by(taxon) %>%
-  summarize(pool = max(rel_abund) < 0.02, 
-            mean = mean(rel_abund),
-            .groups="drop")
-
-inner_join(class_rel_abund, class_pool, by="taxon") %>%
-  mutate(taxon = if_else(pool, "Other", taxon)) %>%
-  group_by(sample, taxon) %>%
-  summarize(rel_abund = 100*sum(rel_abund), 
-            mean = min(mean),
-            .groups="drop") %>%
-  mutate(taxon = factor(taxon),
-         taxon = fct_reorder(taxon, mean, .desc=TRUE),
-         taxon = fct_shift(taxon, n=1)) %>% #all of this mutate step is creating an "anchor" at the top and the bottom of the figure using the 2 most abundant phyla
-  ggplot(aes(x=sample, y=rel_abund, fill=taxon)) +
-  geom_col() 
