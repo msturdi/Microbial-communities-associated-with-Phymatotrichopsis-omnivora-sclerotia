8c2041370850ed2a09425bcbd42212e2aa5a7314
Maxwell Sturdivant <109681165+msturdi@users.noreply.github.com>
2025-07-09 16:29:20 -0500

    Add files via upload

---
+++ Three seasons - Stiles Farm/sig.diff.taxa.ITS.three.R
@@ -0,0 +1,152 @@
+library(tidyverse)
+library(broom)
+library(ggtext)
+library(readxl)
+library(glue)
+library(MoMAColors)
+library(dplyr)
+library(gtools)
+
+set.seed(19980609)
+
+shared <- read_tsv("final.opti_mcc.0.03.subsample.three.fungal.shared",
+                   col_types = cols(Group = col_character(),
+                                    .default = col_double())) %>%
+  rename_all(tolower) %>%
+  select(group, starts_with("otu")) %>%
+  pivot_longer(-group, names_to="otu", values_to="count") %>%
+  mutate(otu = str_replace(string=otu,
+                           pattern="otu",
+                           replacement = "Otu"))
+
+taxonomy <- read_tsv(file="final.opti_mcc.0.03.cons.three.fungal.taxonomy") %>%
+  select("OTU", "Taxonomy") %>%
+  rename_all(tolower) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="\\(\\d*\\)", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern=";$", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="k__", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="p__", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="c__", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="o__", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="f__", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="g__", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="s__", replacement="")) %>%
+  separate(taxonomy, into=c("kingdom", "phylum", "class", "order", "family", "genus"), 
+           sep=";") %>%
+  #mutation code for family or genus level below
+  mutate(pretty_otu = str_replace(string=otu,
+                                  pattern="tu0*",
+                                  replacement = "TU "),
+         order=str_replace(order, "unclassified_(.*)", "Unclassified \\1"),
+         order=str_replace(order, "(.*)_unclassified", "Unclassified \\1"),
+         order=str_replace_all(order, "_", " "))%>%
+  select(otu, order)
+
+metadata <- read_excel("metadata.three.fungal.xlsx") %>%
+  rename(group = sample) %>%
+  mutate(combo = glue("{season} {treatment}"))
+
+composite <- inner_join(shared, taxonomy, by="otu") %>%
+  group_by(group, order) %>%
+  summarize(count = sum(count), .groups="drop") %>%
+  group_by(group) %>%
+  mutate(rel_abund = count / sum(count)) %>%
+  ungroup() %>%
+  select(-count) %>%
+  inner_join(., metadata, by="group")
+
+##
+#for three seasons tables comparing rel abund across the seasons
+##
+composite_ave_scl_trts <- composite %>%
+  subset(rel_abund !="0") %>%
+  mutate(rel_abund = 100 * (rel_abund + 1/20000)) %>%
+  subset(treatment == "live") %>%
+  #subset(treatment == "dead") %>%
+  #subset(treatment == "none") %>%
+  group_by(order, combo) %>%
+  summarize(mean=mean(rel_abund),
+            se = sd(rel_abund) / sqrt(n()),
+            max = mean+se,
+            min = mean-se,
+            N = n(),
+            .groups = "drop")
+
+##
+#stat analysis by sclerotia treatment
+##
+composite_live <- composite %>%
+  subset(treatment == "live")
+
+sig_taxa_live <- composite_live %>%
+  nest(data = -order) %>%
+  mutate(test = map(.x=data, ~pairwise.wilcox.test(x=.x$rel_abund, g=.x$season, p.adjust.method = "BH") %>% tidy())) %>%
+  unnest(test) %>% 
+  filter(p.value < 0.05) %>%
+  #select(genus, p.value) %>%
+  ungroup()
+
+composite_dead <- composite %>%
+  subset(treatment == "dead")
+
+sig_taxa_dead <- composite_dead %>%
+  nest(data = -order) %>%
+  mutate(test = map(.x=data, ~pairwise.wilcox.test(x=.x$rel_abund, g=.x$season, p.adjust.method = "BH") %>% tidy())) %>%
+  unnest(test) %>% 
+  filter(p.value < 0.05) %>%
+  #select(genus, p.value) %>%
+  ungroup()
+
+composite_bulk <- composite %>%
+  subset(treatment == "none")
+
+sig_taxa_bulk <- composite_bulk %>%
+  nest(data = -order) %>%
+  mutate(test = map(.x=data, ~pairwise.wilcox.test(x=.x$rel_abund, g=.x$season, p.adjust.method = "BH") %>% tidy())) %>%
+  unnest(test) %>% 
+  filter(p.value < 0.05) %>%
+  #select(genus, p.value) %>%
+  ungroup()
+
+sig_taxa <- composite %>%
+  nest(data = -order) %>%
+  mutate(test = map(.x=data, ~pairwise.wilcox.test(x=.x$rel_abund, g=.x$season, p.adjust.method = "BH") %>% tidy())) %>%
+  unnest(test) %>% 
+  filter(p.value < 0.05) %>%
+  #select(genus, p.value) %>%
+  ungroup()
+
+#for order
+composite %>%
+  inner_join(sig_taxa, by="order") %>%
+  mutate(rel_abund = 100 * (rel_abund + 1/20000),
+         season = factor(season, levels = c("in23", "off24", "in24"))) %>%
+  ggplot(aes(x=rel_abund, y=order, color=season, fill=season)) +
+  geom_jitter(position = position_jitterdodge(dodge.width = 0.8,
+                                              jitter.width = 0.5),
+              shape=21,
+              size=1.4) +
+  stat_summary(fun.data = median_hilow, fun.args = list(conf.int=0.5),
+               geom="pointrange",
+               position = position_dodge(width=0.1),
+               color="darkgrey", show.legend = FALSE,
+               size = 0.1) +
+  scale_x_log10() +
+  scale_color_manual(name=NULL,
+                     breaks=c("in23", "off24", "in24"),
+                     values=c(moma.colors("Palermo", 3, direction=1, type="discrete")),
+                     labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  scale_fill_manual(name=NULL,
+                    breaks=c("in23", "off24", "in24"),
+                    values=c(moma.colors("Palermo", 3, direction=1, type="discrete")),
+                    labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  labs(x= "Relative abundance", y=NULL,
+       title="Significantly Different Orders (Fungal)<br>Three Seasons at the Stiles Farm") +
+  theme_light() +
+  theme(axis.text.y = element_markdown(size=9),
+        legend.text = element_markdown(size=10),
+        legend.key.size = unit(15, "pt"),
+        legend.position = "bottom",
+        plot.title = element_markdown(hjust=0.5))
+
+ggsave("sig.diff.orders.ITS.three.png", width=13, height=13)
---
+++ Three seasons - Stiles Farm/stat.analysis.ITS.three.R
@@ -0,0 +1,157 @@
+library(tidyverse)
+library(readxl)
+library(purrr)
+library(broom)
+library(dbplyr)
+library(ggtext)
+library(glue)
+library(MoMAColors)
+
+display.all.moma(12, colorblind_only=F)
+
+metadata <- read_excel(path="metadata.three.fungal.xlsx") %>%
+  mutate(combo = glue("{season} {treatment}"))
+
+alpha <- read_tsv(file="final.opti_mcc.groups.ave-std.three.fungal.summary", 
+                  col_types=cols(group = col_character())) %>%
+  filter(method=='ave') %>%
+  select(group, sobs, invsimpson, shannon, shannoneven, coverage)
+
+meta_alpha <- inner_join(metadata, alpha, by=c('sample'='group'))
+
+meta_alpha %>%
+  nest(data = -combo) %>%
+  mutate(summary_data=map(data, ~summary(.x$shannon) %>% tidy)) %>%
+  #mutate(summary_data=map(data, ~summary(.x$invsimpson) %>% tidy)) %>%
+  unnest(cols=summary_data) %>%
+  select(-data)
+
+meta_alpha %>% pull(shannon) %>% shapiro.test()
+meta_alpha %>% pull(sobs) %>% shapiro.test() 
+meta_alpha %>% pull(shannoneven) %>% shapiro.test() 
+
+#tests for homoscedasticity
+bartlett.test(shannon~season, data=meta_alpha)
+bartlett.test(sobs~season, data=meta_alpha)
+bartlett.test(shannoneven~season, data=meta_alpha)
+
+#assessing sig. of metrics because they are all non-normal but homoscedastic 
+kruskal.test(shannon~season, data=meta_alpha)
+pairwise.wilcox.test(g=meta_alpha$season, x=meta_alpha$shannon, p.adjust.method="BH")
+
+kruskal.test(sobs~season, data=meta_alpha)
+pairwise.wilcox.test(g=meta_alpha$season, x=meta_alpha$sobs, p.adjust.method="BH")
+
+kruskal.test(shannoneven~season, data=meta_alpha)
+pairwise.wilcox.test(g=meta_alpha$season, x=meta_alpha$shannoneven, p.adjust.method="BH")
+
+#shannon season only
+meta_alpha %>%
+  ggplot(aes(x=season, y=shannon, color=season)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(aes(fill=season), position = position_jitterdodge(dodge.width = 0.3, jitter.width = 0.3), shape=21, size=1.5) +
+  stat_summary(fun.y = mean, geom="point", shape=23, size=2) + 
+  scale_color_manual(name=NULL,
+                     breaks=c("in23", "off24", "in24"),
+                     values=c(moma.colors("Palermo", 3, direction=1, type="discrete")),
+                     labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  scale_fill_manual(name=NULL,
+                    breaks=c("in23", "off24", "in24"),
+                    values=c(moma.colors("Palermo", 3, direction=1, type="discrete")),
+                    labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  scale_x_discrete(limits=c("in23", "off24", "in24"),
+                   labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  labs(x=NULL,
+       y="Shannon",
+       title="Comparison of Three Seasons of Fungal Shannon Diversity<br>Stiles Farm") +
+  theme_classic() +
+  theme(axis.text.x = element_markdown(size=10),
+        axis.text.y = element_markdown(size=10),
+        legend.position = "none",
+        plot.title = element_markdown(hjust=0.5, size=12, vjust=2))
+
+ggsave("shannon.ITS.three.png", width=8, height=6)
+
+#just season sobs
+meta_alpha %>%
+  ggplot(aes(x=season, y=sobs, color=season)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(aes(fill=season), position = position_jitterdodge(dodge.width = 0.3, jitter.width = 0.3), shape=21, size=1.5) +
+  stat_summary(fun.y = mean, geom="point", shape=23, size=2) + 
+  scale_color_manual(name=NULL,
+                     breaks=c("in23", "off24", "in24"),
+                     values=c(moma.colors("Palermo", 3, direction=1, type="discrete")),
+                     labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  scale_fill_manual(name=NULL,
+                    breaks=c("in23", "off24", "in24"),
+                    values=c(moma.colors("Palermo", 3, direction=1, type="discrete")),
+                    labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  scale_x_discrete(limits=c("in23", "off24", "in24"),
+                   labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  labs(x=NULL,
+       y="Observed Diversity",
+       title="Comparison of Three Seasons of Fungal Observed Diversity<br>Stiles Farm") +
+  theme_classic() +
+  theme(axis.text.x = element_markdown(size=10),
+        axis.text.y = element_markdown(size=10),
+        legend.position = "none",
+        plot.title = element_markdown(hjust=0.5, size=12, vjust=2))
+
+ggsave("sobs.ITS.three.png", width=8, height=6)
+
+#just season evenness 
+meta_alpha %>%
+  ggplot(aes(x=season, y=shannoneven, color=season)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(aes(fill=season), position = position_jitterdodge(dodge.width = 0.3, jitter.width = 0.3), shape=21, size=1.5) +
+  stat_summary(fun.y = mean, geom="point", shape=23, size=2) + 
+  scale_color_manual(name=NULL,
+                     breaks=c("in23", "off24", "in24"),
+                     values=c(moma.colors("Palermo", 3, direction=1, type="discrete")),
+                     labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  scale_fill_manual(name=NULL,
+                    breaks=c("in23", "off24", "in24"),
+                    values=c(moma.colors("Palermo", 3, direction=1, type="discrete")),
+                    labels=c("In-season '23", "Off-season '24", "In-season '24")) +
+  scale_x_discrete(limits=c("in23", "off24", "in24"),
+                   labels=c("In-season '23", "Off-season '24", "In-season '24"))  +
+  labs(x=NULL,
+       y="Shannon Evenness",
+       title="Comparison of Three Seasons of Fungal Evenness<br>Stiles Farm") +
+  theme_classic() +
+  theme(axis.text.x = element_markdown(size=10),
+        axis.text.y = element_markdown(size=10),
+        legend.position = "none",
+        plot.title = element_markdown(hjust=0.5, size=12, vjust=2))
+
+ggsave("even.ITS.three.png", width=8, height=6)
+
+#sclerotia trts
+meta_alpha %>%
+  ggplot(aes(x=combo, y=shannon, color=combo)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(aes(fill=combo), position = position_jitterdodge(dodge.width = 0.3, jitter.width = 0.3), shape=21, size=1.5) +
+  stat_summary(fun.y = mean, geom="point", shape=23, size=2) + 
+  scale_color_manual(name=NULL,
+                     breaks=c("in23 live", "in23 dead", "in23 none", "off24 live", "off24 dead", "off24 none", "in24 live", "in24 dead", "in24 none"),
+                     values=c(moma.colors("OKeeffe", 9, direction=1, type="continuous")),
+                     labels=c("In-season '23<br>live", "In-season '23<br>heat-killed", "In-season '23<br>bulk", "Off-season '24<br>live", "Off-season '24<br>heat-killed", 
+                              "Off-season '24<br>bulk", "In-season '24<br>live", "In-season '24<br>heat-killed", "In-season '24<br>bulk")) +
+  scale_fill_manual(name=NULL,
+                    breaks=c("in23 live", "in23 dead", "in23 none", "off24 live", "off24 dead", "off24 none", "in24 live", "in24 dead", "in24 none"),
+                    values=c(moma.colors("OKeeffe", 9, direction=1, type="continuous")),
+                    labels=c("In-season '23<br>live", "In-season '23<br>heat-killed", "In-season '23<br>bulk", "Off-season '24<br>live", "Off-season '24<br>heat-killed", 
+                             "Off-season '24<br>bulk", "In-season '24<br>live", "In-season '24<br>heat-killed", "In-season '24<br>bulk"))  +
+  scale_x_discrete(limits=c("in23 live", "in23 dead", "in23 none", "off24 live", "off24 dead", "off24 none", "in24 live", "in24 dead", "in24 none"),
+                   labels=c("In-season '23<br>live", "In-season '23<br>heat-killed", "In-season '23<br>bulk", "Off-season '24<br>live", "Off-season '24<br>heat-killed", 
+                            "Off-season '24<br>bulk", "In-season '24<br>live", "In-season '24<br>heat-killed", "In-season '24<br>bulk")) +
+  labs(x=NULL,
+       y="Shannon",
+       title="Comparison of Three Seasons of Fungal Diversity<br>Stiles Farm") +
+  theme_classic() +
+  theme(axis.text.x = element_markdown(size=10),
+        axis.text.y = element_markdown(size=10),
+        legend.position = "none",
+        plot.title = element_markdown(hjust=0.5, size=12, vjust=2))
+
+ggsave("shannon.ITS.three.trt.png", width=10, height=6)
