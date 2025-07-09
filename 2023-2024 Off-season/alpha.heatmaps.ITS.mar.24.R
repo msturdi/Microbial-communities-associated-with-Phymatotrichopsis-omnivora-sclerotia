cc1dde49bdf34e4a8fd82484cc3d9c256a7addfd
Maxwell Sturdivant <109681165+msturdi@users.noreply.github.com>
2025-07-09 15:28:06 -0500

    Update alpha.heatmaps.ITS.mar.24.R

--- 2023-2024 Off-season/alpha.heatmaps.ITS.mar.24.R
+++ 2023-2024 Off-season/alpha.heatmaps.ITS.mar.24.R
@@ -1,8 +1,6 @@
 library(tidyverse)
 library(readxl)
 library(ggtext)
-library(RColorBrewer)
-library(MoMAColors)
 library(glue)
 library(scales)
 
@@ -98,7 +96,7 @@ order_rel_abund %>%
   scale_fill_gradient(low="white", high="red",
                       name=("Relative<br>Abundance (%)"),
                       limits=c(0,50), 
-                      oob=squish, #part of "scales" library
+                      oob=squish,
                       breaks = c(0, 10, 20, 30, 40, 50), labels = c("0", "10", "20", "30", "40", ">50")) + 
   coord_fixed() +
   scale_x_discrete(limits=c("BFO1A1F", "BFO1A2F", "BFO1B1F", "BFO1B2F", "BFO2A1F", "BFO2A2F", "BFO2B1F", "BFO2B2F", "BFO3A1F", "BFO3A2F", "BFO3B1F", "BFO3B2F",
@@ -128,7 +126,6 @@ order_rel_abund %>%
         axis.ticks = element_blank(), #gets rid of the axis tick marks
         axis.text.y = element_markdown(size=8),
         axis.text.x = element_markdown(size=8),
-        #legend.title.align = 0.5,
         legend.title = element_markdown(size=8),
         legend.text = element_text(size=7.5),
         legend.key.height = unit(11, "pt"),
@@ -146,7 +143,7 @@ order_rel_abund_mean %>%
   scale_fill_gradient(low="white", high="red",
                       name=("Average Relative<br>Abundance (%)"),
                       limits=c(0,50), 
-                      oob=squish, #part of "scales" library
+                      oob=squish,
                       breaks = c(0, 10, 20, 30, 40, 50), labels = c("0", "10", "20", "30", "40", ">50")) + 
   coord_fixed() +
   scale_x_discrete(limits=c("BF live", "BF dead", "BF none", "Stiles live", "Stiles dead", "Stiles none"),
@@ -170,85 +167,3 @@ order_rel_abund_mean %>%
         plot.title = element_markdown(hjust=0.5, size=14))
 
 ggsave("heatmap.ITS.order.ave.png", width=12, height=10)
-
-#genus-level relative abundance
-genus_rel_abund <- otu_rel_abund %>%
-  filter(level=="genus") %>%
-  group_by(location, sample, taxon) %>%
-  summarize(rel_abund = sum(rel_abund), 
-            .groups="drop") %>%
-  group_by(location, taxon) %>%
-  mutate(taxon = str_replace(taxon,
-                             "(.*)_unclassified", "Unclassified *\\1*"),
-         taxon = str_replace(taxon,
-                             "^(\\S*)$", "*\\1*"),
-         taxon = str_replace_all(taxon, 
-                                 "_", " ")) #gets ride of _ in taxon names
-
-genus_rel_abund %>%
-  group_by(taxon) %>%
-  summarize(rel_abund=sum(rel_abund)) %>%
-  arrange(rel_abund) %>%
-  view()
-
-top_genus <- genus_rel_abund %>%
-  group_by(taxon) %>%
-  summarize(rel_abund = sum(rel_abund)) %>%
-  arrange(rel_abund) %>%
-  top_n(12, rel_abund) %>%
-  pull(taxon)
-view(top_genus)
-
-genus_rel_abund %>%
-  filter(taxon %in% top_genus) %>%
-  mutate(taxon=factor(taxon, levels=top_genus)) %>%
-  mutate(rel_abund=100 * rel_abund)
-
-genus_rel_abund %>%
-  filter(taxon %in% top_genus) %>%
-  mutate(taxon=factor(taxon, levels=top_genus)) %>%
-  mutate(rel_abund= 100 * (rel_abund + 1/20000)) %>%
-  ggplot(aes(x=sample, y=taxon, fill=rel_abund)) +
-  geom_tile(color="black") +
-  scale_fill_gradient(low="white", high="red",
-                      name=("Relative<br>Abundance (%)"),
-                      limits=c(0,50), 
-                      oob=squish, #part of "scales" library
-                      breaks = c(0, 10, 20, 30, 40, 50), labels = c("0", "10", "20", "30", "40", ">50")) + 
-  coord_fixed() +
-  scale_x_discrete(limits=c("BFO1A1F", "BFO1A2F", "BFO1B1F", "BFO1B2F", "BFO2A1F", "BFO2A2F", "BFO2B1F", "BFO2B2F", "BFO3A1F", "BFO3A2F", "BFO3B1F", "BFO3B2F",
-                            "BFO1C1F", "BFO1C2F", "BFO2C1F", "BFO2C2F", "BFO3C1F", "BFO3C2F",
-                            "BFOBA1F", "BFOBA2F", "BFOBB1F", "BFOBB2F",
-                            "SO1A1F", "SO1A2F", "SO1B1F", "SO1B2F", "SO2A1F", "SO2A2F", "SO2B1F", "SO2B2F", "SO3A1F", "SO3A2F", "SO3B1F", "SO3B2F",
-                            "SO1C1F", "SO1C2F", "SO2C1F", "SO2C2F", "SO3C1F", "SO3C2F",
-                            "SOBA1F", "SOBA2F", "SOBB1F", "SOBB2F"),
-                   labels=c("BF<br>Live<br>1", "BF<br>Live<br>2", "BF<br>Live<br>3", "BF<br>Live<br>4", "BF<br>Live<br>5","BF<br>Live<br>6", "BF<br>Live<br>7", "BF<br>Live<br>8", 
-                            "BF<br>Live<br>9", "BF<br>Live<br>10", "BF<br>Live<br>11", "BF<br>Live<br>12",
-                            "BF<br>Heat<br>Killed<br>1", "BF<br>Heat<br>Killed<br>2", "BF<br>Heat<br>Killed<br>3", "BF<br>Heat<br>Killed<br>4", "BF<br>Heat<br>Killed<br>5", 
-                            "BF<br>Heat<br>Killed<br>6",
-                            "BF<br>Bulk<br>Soil<br>1", "BF<br>Bulk<br>Soil<br>2", "BF<br>Bulk<br>Soil<br>3", "BF<br>Bulk<br>Soil<br>4",
-                            "SF<br>Live<br>1", "SF<br>Live<br>2", "SF<br>Live<br>3", "SF<br>Live<br>4", "SF<br>Live<br>5","SF<br>Live<br>6", "SF<br>Live<br>7", "SF<br>Live<br>8", 
-                            "SF<br>Live<br>9", "SF<br>Live<br>10", "SF<br>Live<br>11", "SF<br>Live<br>12",
-                            "SF<br>Heat<br>Killed<br>1", "SF<br>Heat<br>Killed<br>2", "SF<br>Heat<br>Killed<br>3", "SF<br>Heat<br>Killed<br>4", "SF<br>Heat<br>Killed<br>5", 
-                            "SF<br>Heat<br>Killed<br>6",
-                            "SF<br>Bulk<br>Soil<br>1", "SF<br>Bulk<br>Soil<br>2", "SF<br>Bulk<br>Soil<br>3", "SF<br>Bulk<br>Soil<br>4")) +
-  scale_y_discrete(breaks=c("Unclassified Nectriaceae", "*Aspergillus*", "*Alternaria*", "Unclassified Fungi", "Unclassified Ascomycota", "Unclassified Sordariaceae", "*Phoma*",
-                            "*Talaromyces*", "*Penicillium*", "*Stagonosporopsis*", "*Macrophomina*", "*Trichoderma*"),
-                   labels=c("**Unclassified Nectriaceae (BF)***", "*Aspergillus*", "***Alternaria* (SF)***", "Unclassified Fungi", "**Unclassified Ascomycota (BF)***", 
-                            "**Unclassified Sordariaceae (S)***", "*Phoma*", "***Talaromyces* (S)***", "***Penicillium* (S)***", "***Stagonosporopsis* (BF)***", "*Macrophomina*", 
-                            "***Trichoderma* (BF)***")) +
-  labs(title="Relative Abundance of the Top 12 Genera",
-       x=NULL,
-       y=NULL) +
-  theme_classic() +
-  theme(axis.line = element_blank(), #gets rid of the axis lines
-        axis.ticks = element_blank(), #gets rid of the axis tick marks
-        axis.text.y = element_markdown(size=8),
-        axis.text.x = element_markdown(size=8),
-        #legend.title.align = 0.5,
-        legend.title = element_markdown(size=8),
-        legend.text = element_text(size=7.5),
-        legend.key.height = unit(11, "pt"),
-        plot.title = element_text(hjust=0.5))
-
-ggsave("heatmap.ITS.genus.png", width=17, height=7)
