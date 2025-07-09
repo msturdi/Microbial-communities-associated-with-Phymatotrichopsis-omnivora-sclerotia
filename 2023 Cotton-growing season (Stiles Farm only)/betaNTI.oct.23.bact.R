f41cdb7e0f0373ca603fd117a7c9576fa54587d3
Maxwell Sturdivant <109681165+msturdi@users.noreply.github.com>
2025-07-09 14:47:35 -0500

    Update betaNTI.oct.23.bact.R

--- 2023 Cotton-growing season (Stiles Farm only)/betaNTI.oct.23.bact.R
+++ 2023 Cotton-growing season (Stiles Farm only)/betaNTI.oct.23.bact.R
@@ -9,31 +9,9 @@ library(file2meco)
 library(dplyr)
 library(metagenomeSeq)
 library(iCAMP)
-library(MoMAColors)
 library(ggtext)
 
 set.seed(19980609)
-
-display.all.moma(6, colorblind_only=T)
-
-##
-#filtering out single otus in shared file
-##
-#shared <- read_tsv("final.opti_mcc.0.03.subsample.oct.24.bacterial.shared",
-#col_types = cols(Group = col_character(),
-#.default = col_double()))
-
-#meta_cols <- c("label", "Group", "numOtus") #define columns to keep from shared file
-#otu_sums <- colSums(shared[, !(names(shared) %in% meta_cols)]) #sum otu abundances across all samples
-#otus_to_keep <- names(otu_sums[otu_sums > 1]) #keep otus with total counts above 1
-
-#shared_filtered <- shared %>%
-#select(all_of(meta_cols), all_of(otus_to_keep))
-
-#otu_count <- shared_filtered %>%
-#select(Group, starts_with("Otu")) %>%
-#rename(sample = Group) %>%
-#column_to_rownames(var = "sample")
 
 otu_count <- read_tsv("final.opti_mcc.0.03.subsample.shared") %>%
   select(Group, starts_with("Otu")) %>%
@@ -114,21 +92,16 @@ physeq
 
 meco_dataset <- phyloseq2meco(physeq)
 
-##
-#Code from Vanessa to filter out very low abundance OTUs
-##
 meco_dataset$filter_taxa(rel_abund = 0.00001, freq = 0.1)
 meco_dataset
 
-#allow you to calculate beta NTI and other phylogenetic metrics. Use a threshold to filter low-abundance taxa if needed.
-#beta:method = "CSS" (Cumulative Sum Scaling)
 dt <- trans_norm$new(meco_dataset)
 dt2 <- dt$norm(method = "CSS")
 
 t1 <- trans_nullmodel$new(dt2, filter_thres = 0.000005)
 t1$data_tree
 
-t1$cal_ses_betamntd(runs = 999, abundance.weighted = TRUE, null.model = "sample.pool") #runs should = 999, trying 99 to see how long it takes
+t1$cal_ses_betamntd(runs = 999, abundance.weighted = TRUE, null.model = "sample.pool")
 t1$res_ses_betamntd
 
 #View and store beta NTI results
@@ -154,75 +127,8 @@ t2$sample_table$trt <- factor(t2$sample_table$trt,
 t2$sample_table$treatment <- factor(t2$sample_table$treatment, 
                                     levels = c("live", "dead", "none"))
 
-##
-#make CSV file so that you don't have to run the 999 step again
-##
-g1 <- t2$plot_group_distance(boxplot_add = "mean") #in g1 figure, above -1 is stochastic and below is deterministic
+g1 <- t2$plot_group_distance(boxplot_add = "mean")
 
 plot_data <- g1$data
 
 write.csv(plot_data, file = "group.distance.oct.23.bact.csv", row.names = FALSE)
-##
-
-g2 = g1 +
-  scale_color_manual(name=NULL,
-                     breaks=c("live", "dead", "none"),
-                     labels=c("Live sclerotia", "Heat-killed sclerotia", "Bulk soil"),
-                     values = c(moma.colors("Sidhu", 3, direction=1, type="discrete"))) +
-  scale_x_discrete(limits=c("live", "dead", "none"),
-                   labels=c("Live sclerotia", "Heat-killed sclerotia", "Bulk soil")) +
-  labs(x=NULL,
-       y="betaNTI",
-       title="Stochastic or Deterministic Selection<br>In-season 2023 Stiles Farm **Bacterial**") +
-  theme_classic() +
-  theme(axis.text.x = element_markdown(size = 14, color = "black"),
-        axis.text.y = element_markdown(size = 12),
-        axis.title.y = element_markdown(size = 14),
-        legend.position = "none",
-        plot.title = element_markdown(hjust=0.5, size = 16))
-
-ggsave("betaNTI.oct23.16S.png", width=12, height=8)
-
-##
-#from Vanessa's code, but doesn't seem to take into account the -2 threshold
-##
-#t2$res_group_distance$Process <- ifelse(
-#abs(t2$res_group_distance$Value) > 2.0, "Deterministic", "Stochastic")
-
-t2$res_group_distance$Process <- ifelse(t2$res_group_distance$Value > 2.0 | t2$res_group_distance$Value < -2.0, 
-                                        "Deterministic", "Stochastic")
-
-proportions <- t2$res_group_distance %>%
-  group_by(trt, Process) %>%
-  summarize(Count = n()) %>%
-  mutate(Proportion = Count*100 / sum(Count)) %>%
-  arrange(trt, Process)
-
-proportions$trt <- factor(proportions$trt, 
-                          levels = c("live", "dead", "none"))
-
-proportions %>%
-  ggplot(aes(x = trt, y = Proportion, fill = Process)) +
-  geom_col() +
-  scale_fill_manual(name="Selection Type",
-                    breaks=c("Deterministic", "Stochastic"),
-                    values=c('cadetblue3','coral2'),
-                    labels=c("Deterministic", "Stochastic")) +
-  scale_x_discrete(limits=c("BF live", "BF dead", "BF none", "Stiles live", "Stiles dead", "Stiles none"),
-                   labels=c("BF live<br>sclerotia", "BF heat-killed<br>sclerotia", "BF bulk", 
-                            "SF live<br>sclerotia", "SF heat-killed<br>sclerotia", "SF bulk")) +
-  scale_y_continuous(expand=c(0,0)) +
-  labs(x=NULL,
-       y="Stochastic vs. Deterministic Selection",
-       title="Proportion of Stochastic vs. Deterministic Selection<br>In-season 2024 **Fungal**") +
-  theme_classic() +
-  theme(axis.text.x = element_markdown(size = 14, color = "black"),
-        axis.text.y = element_markdown(size = 10),
-        axis.title.y = element_markdown(size = 14),
-        legend.position = "bottom",
-        legend.text = element_markdown(size = 10),
-        legend.title = element_text(size=11),
-        legend.key.size = unit(10, "pt"),
-        plot.title = element_markdown(hjust=0.5, size = 16))
-
-ggsave("porportion.selection.oct24.ITS.png", width=12, height=8)
